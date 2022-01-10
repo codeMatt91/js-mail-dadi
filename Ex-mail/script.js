f5ae373fd3dea0e1f8cd31110d603a82ee9e431e
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-10 15:10:39 +0100

    Created 2 folders for each exercises

---
+++ Ex-mail/script.js
@@ -0,0 +1,19 @@
+/* 
+
+# Mail
+Crea un array di email;
+Chiedi all’utente la sua email.
+controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+
+1- creare array
+2- chiedi all'utente di inserire la sua mail
+3-verificare che la mail sia nell'array
+4-stampa l'esito 
+
+*/
+
+console.log('JS ok!');
+
+const emailList = ['matteo.imbimbo@yahoo.it', 'marina.92@gmail.com', 'valentina91@libero.it', 'giulioleo@hotmail.com', 'giulia89@gmail.com'];
+
+const userEmail = prompt('inserisci la tua mail:')
