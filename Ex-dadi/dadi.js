9f6a0d266d6c942335e30cf78cc27a5fcb914a4b
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-10 15:54:35 +0100

    added files in folder dadi

---
+++ Ex-dadi/dadi.js
@@ -0,0 +1,13 @@
+/* 
+
+ # Gioco dei dadi
+ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+ Stabilire il vincitore, in base a chi fa il punteggio più alto.
+ Stampare in console i tiri di dado e il risultato.
+
+ 1- Generare un numero random per l' utente da 1 a 6
+ 2- generare un numero random per il PC
+ 3- confrontare i numeri 
+ 4- stabilire il vincitore 
+ 5- stamparlo in pagina
+*/
